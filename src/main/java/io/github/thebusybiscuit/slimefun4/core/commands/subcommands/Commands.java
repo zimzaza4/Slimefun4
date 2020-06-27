fda4074815f4bd88336e891bd3ef89bc3e82d72f
Name1ess <starwishsama@outlook.com>
2020-06-27 14:28:19 +0800

    Update Commands.java

--- src/main/java/io/github/thebusybiscuit/slimefun4/core/commands/subcommands/Commands.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/core/commands/subcommands/Commands.java
@@ -27,5 +27,6 @@ public final class Commands {
         commands.add(new SearchCommand(plugin, cmd));
         commands.add(new DebugFishCommand(plugin, cmd));
         commands.add(new BackpackCommand(plugin, cmd));
+        commands.add(new ItemIdCommand(plugin, cmd))
     }
 }
