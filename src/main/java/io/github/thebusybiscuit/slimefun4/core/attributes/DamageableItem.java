f387e89cf4b6cb261ba53ee91f1253891b1f8026
StarWishsama <starwishsama@outlook.com>
2020-09-05 16:01:29 +0800

    merge at c5630791c06e56d63b55885813672088fce973d0

--- src/main/java/io/github/thebusybiscuit/slimefun4/core/attributes/DamageableItem.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/core/attributes/DamageableItem.java
@@ -1,18 +1,22 @@
 package io.github.thebusybiscuit.slimefun4.core.attributes;
 
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.Damageable;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
 @FunctionalInterface
 public interface DamageableItem {
 
     boolean isDamageable();
 
-    default void damageItem(Player p, ItemStack item) {
+    default void damageItem(@Nonnull Player p, @Nullable ItemStack item) {
         if (isDamageable() && item != null && item.getType() != Material.AIR && item.getAmount() > 0) {
             if (item.getEnchantments().containsKey(Enchantment.DURABILITY) && Math.random() * 100 <= (60 + Math.floorDiv(40, (item.getEnchantmentLevel(Enchantment.DURABILITY) + 1)))) {
                 return;
@@ -22,6 +26,7 @@ public interface DamageableItem {
             Damageable damageable = (Damageable) meta;
 
             if (damageable.getDamage() >= item.getType().getMaxDurability()) {
+                p.playSound(p.getEyeLocation(), Sound.ENTITY_ITEM_BREAK, 1, 1);
                 item.setAmount(0);
             } else {
                 damageable.setDamage(damageable.getDamage() + 1);
--- src/main/java/io/github/thebusybiscuit/slimefun4/implementation/setup/SlimefunItemSetup.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/implementation/setup/SlimefunItemSetup.java
@@ -997,6 +997,11 @@ public final class SlimefunItemSetup {
                 new ItemStack[]{SlimefunItems.ENDER_LUMP_3, SlimefunItems.AIR_RUNE, SlimefunItems.ENDER_LUMP_3, SlimefunItems.EARTH_RUNE, SlimefunItems.NECROTIC_SKULL, SlimefunItems.FIRE_RUNE, SlimefunItems.ENDER_LUMP_3, SlimefunItems.WATER_RUNE, SlimefunItems.ENDER_LUMP_3})
                 .register(plugin);
 
+        new SlimefunItem(categories.magicalResources, SlimefunItems.SYNTHETIC_SHULKER_SHELL, RecipeType.ANCIENT_ALTAR,
+                new ItemStack[]{SlimefunItems.ENDER_LUMP_3, SlimefunItems.ENDER_RUNE, SlimefunItems.ENDER_LUMP_3, SlimefunItems.REINFORCED_PLATE, new ItemStack(Material.TURTLE_HELMET), SlimefunItems.REINFORCED_PLATE, SlimefunItems.ENDER_LUMP_3, SlimefunItems.ENDER_RUNE, SlimefunItems.ENDER_LUMP_3})
+                .setUseableInWorkbench(true)
+                .register(plugin);
+
         new SoulboundBackpack(36, categories.magicalGadgets, SlimefunItems.BOUND_BACKPACK, RecipeType.MAGIC_WORKBENCH,
                 new ItemStack[]{SlimefunItems.ENDER_LUMP_2, null, SlimefunItems.ENDER_LUMP_2, SlimefunItems.ESSENCE_OF_AFTERLIFE, SlimefunItems.WOVEN_BACKPACK, SlimefunItems.ESSENCE_OF_AFTERLIFE, SlimefunItems.ENDER_LUMP_2, null, SlimefunItems.ENDER_LUMP_2})
                 .register(plugin);
