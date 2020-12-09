08d7482805d9fb587a0c563ea62d2966bb1f53f6
StarWishsama <starwishsama@outlook.com>
2020-12-09 12:13:56 +0800

    U: Working on OverclockModule

--- src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/electric/gadgets/OverclockModule.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/electric/gadgets/OverclockModule.java
@@ -2,8 +2,10 @@ package io.github.thebusybiscuit.slimefun4.implementation.items.electric.gadgets
 
 import io.github.thebusybiscuit.slimefun4.core.handlers.ItemUseHandler;
 import io.github.thebusybiscuit.slimefun4.implementation.items.SimpleSlimefunItem;
+import me.mrCookieSlime.CSCoreLibPlugin.Configuration.Config;
 import me.mrCookieSlime.Slimefun.Lists.RecipeType;
 import me.mrCookieSlime.Slimefun.Objects.Category;
+import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.SlimefunItem;
 import me.mrCookieSlime.Slimefun.api.BlockStorage;
 import me.mrCookieSlime.Slimefun.api.SlimefunItemStack;
 import org.bukkit.block.Block;
@@ -12,6 +14,8 @@ import org.bukkit.inventory.ItemStack;
 import java.util.Optional;
 
 public class OverclockModule extends SimpleSlimefunItem<ItemUseHandler> {
+    private final String OVERCLOCK_PATH = "overclock-multiply";
+
     public OverclockModule(Category category, SlimefunItemStack item, RecipeType recipeType, ItemStack[] recipe) {
         super(category, item, recipeType, recipe);
     }
@@ -20,18 +24,23 @@ public class OverclockModule extends SimpleSlimefunItem<ItemUseHandler> {
     public ItemUseHandler getItemHandler() {
         return e -> {
             Optional<Block> block = e.getClickedBlock();
+            e.cancel();
 
             if (block.isPresent()) {
                 Block b = block.get();
 
-                if (BlockStorage.hasBlockInfo(b)) {
-                    e.cancel();
+                String json = BlockStorage.getBlockInfoAsJson(b);
+                e.getPlayer().sendMessage(json);
 
-                    if (e.getPlayer().isOp()) {
-                        String json = BlockStorage.getBlockInfoAsJson(b);
-                        e.getPlayer().sendMessage(json);
+                if (SlimefunItem.getByID("") != null && BlockStorage.hasBlockInfo(b)) {
+                    Config cfg = BlockStorage.getLocationInfo(b.getLocation());
+                    int multiply = cfg.getInt(OVERCLOCK_PATH);
+
+                    if (multiply >= 10) {
+                        e.getPlayer().sendMessage("超频倍率已达上限: 10x");
                     } else {
-                        e.getPlayer().sendMessage("这个物品还没有实装");
+                        cfg.setValue(OVERCLOCK_PATH, multiply + 1);
+                        e.getPlayer().sendMessage("超频机器成功, 目前速度: ");
                     }
                 }
             }
