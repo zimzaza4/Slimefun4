698d716517fc508d002f9d7f0a01b9b94944e650
StarWishsama <starwishsama@outlook.com>
2021-02-10 18:38:19 +0800

    trans: 调整翻译

--- src/main/java/io/github/starwishsama/utils/LangUtil.java
+++ src/main/java/io/github/starwishsama/utils/LangUtil.java
@@ -30,14 +30,14 @@ public class LangUtil {
         if (System.getProperty(benefitsProperty) == null) {
             System.setProperty(benefitsProperty, "1");
             logger.warning("  ");
-            logger.warning(" Paper 提供显而易见的性能优化,");
-            logger.warning(" Bug 修复, 更好的安全性, 可选的部分特性");
-            logger.warning(" 为服主们提升TA们的服务器体验.");
+            logger.warning(" Paper 能提供显著性能优化, 且更安全,");
+            logger.warning(" 以及 Bug 修复和部分新特性");
+            logger.warning(" 提升服主的服务器体验.");
             logger.warning("  ");
             logger.warning(" Paper 内置了 Timings v2. 相比 v1 版本");
-            logger.warning(" 能够更显著地诊断服务器卡顿.");
+            logger.warning(" 能够更显著地诊断服务器卡顿原因.");
             logger.warning("  ");
-            logger.warning(" 你原有的插件在更换后应该仍能正常使用.");
+            logger.warning(" 你原有的插件在更换后大部分都能正常使用.");
             logger.warning(" 如果遇到问题, Paper 社区很乐意帮助你解决你的问题.");
             logger.warning("  ");
             logger.warning(" 加入 Paper 社区 @ https://papermc.io");
