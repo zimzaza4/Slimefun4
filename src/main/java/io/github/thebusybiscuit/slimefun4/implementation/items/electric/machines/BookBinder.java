5418596930187562f953f257a55ce4687ccf026a
StarWishsama <starwishsama@outlook.com>
2021-03-07 14:21:16 +0800

    merge: https://github.com/Slimefun/Slimefun4/commit/d33dc965e0b7b2cc7ce48cfb4c6461843ec593aa

--- src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/electric/machines/BookBinder.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/electric/machines/BookBinder.java
@@ -44,10 +44,9 @@ public class BookBinder extends AContainer {
     protected MachineRecipe findNextRecipe(BlockMenu menu) {
         for (int slot : getInputSlots()) {
             ItemStack target = menu.getItemInSlot(slot == getInputSlots()[0] ? getInputSlots()[1] : getInputSlots()[0]);
-
             ItemStack item = menu.getItemInSlot(slot);
-            if  (isCompatible(item) && isCompatible(target)) {
 
+            if (isCompatible(item) && isCompatible(target)) {
                 EnchantmentStorageMeta itemMeta = (EnchantmentStorageMeta) item.getItemMeta();
                 EnchantmentStorageMeta targetMeta = (EnchantmentStorageMeta) target.getItemMeta();
 
@@ -55,17 +54,24 @@ public class BookBinder extends AContainer {
                 Map<Enchantment, Integer> storedTargetEnchantments = targetMeta.getStoredEnchants();
                 Map<Enchantment, Integer> enchantments = combineEnchantments(storedItemEnchantments, storedTargetEnchantments);
 
+                // Just return if no enchantments exist. This shouldn't ever happen. :NotLikeThis:
                 if (enchantments.size() > 0) {
                     ItemStack book = new ItemStack(Material.ENCHANTED_BOOK);
 
                     EnchantmentStorageMeta enchantMeta = (EnchantmentStorageMeta) book.getItemMeta();
+
                     for (Map.Entry<Enchantment, Integer> entry : enchantments.entrySet()) {
                         enchantMeta.addStoredEnchant(entry.getKey(), entry.getValue(), bypassVanillaMaxLevel.getValue());
                     }
 
+                    // Make sure we never return an enchanted book with no enchantments.
+                    if (enchantMeta.getStoredEnchants().isEmpty()) {
+                        return null;
+                    }
+
                     book.setItemMeta(enchantMeta);
 
-                    MachineRecipe recipe = new MachineRecipe(25 * (enchantments.size() / this.getSpeed()), new ItemStack[] {target, item}, new ItemStack[] {book});
+                    MachineRecipe recipe = new MachineRecipe(25 * (enchantments.size() / this.getSpeed()), new ItemStack[] { target, item }, new ItemStack[] { book });
 
                     if (!InvUtils.fitAll(menu.toInventory(), recipe.getOutput(), getOutputSlots())) {
                         return null;
@@ -105,28 +111,38 @@ public class BookBinder extends AContainer {
     private Map<Enchantment, Integer> combineEnchantments(Map<Enchantment, Integer> ech1, Map<Enchantment, Integer> ech2) {
         Map<Enchantment, Integer> enchantments = new HashMap<>();
         boolean conflicts = false;
-
         enchantments.putAll(ech1);
+
         for (Map.Entry<Enchantment, Integer> entry : ech2.entrySet()) {
             for (Map.Entry<Enchantment, Integer> conflictsWith : enchantments.entrySet()) {
-                if (entry.getKey().conflictsWith(conflictsWith.getKey())) {
+
+                // Check if entry enchantment and conflictsWith enchantment conflict, and confirm that the enchantsments aren't the exact same.
+                if (entry.getKey().conflictsWith(conflictsWith.getKey()) && !entry.getKey().equals(conflictsWith.getKey())) {
                     conflicts = true;
                 }
             }
 
             if (!conflicts) {
                 enchantments.merge(entry.getKey(), entry.getValue(), (a, b) -> {
-                    if (a == b) {
-                        if (hasCustomMaxLevel.getValue()) {
+                    int enchantMaxLevel = entry.getKey().getMaxLevel();
+
+                    if (a.intValue() == b.intValue()) {
+
+                        // Confirm the entry's enchant level doesn't go over the maximum unless it uses bypass-vanilla-max-level
+                        if (enchantMaxLevel <= a && !bypassVanillaMaxLevel.getValue()) {
+                            return enchantMaxLevel;
+                        } else if (hasCustomMaxLevel.getValue()) {
                             return a + 1 > customMaxLevel.getValue() ? customMaxLevel.getValue() : a + 1;
                         } else {
                             return a + 1;
                         }
-
                     } else {
                         int highestLevel = Math.max(a, b);
 
-                        if (hasCustomMaxLevel.getValue()) {
+                        // Confirm the entry's enchant level doesn't go over the maximum unless it uses bypass-vanilla-max-level
+                        if (enchantMaxLevel <= highestLevel && !bypassVanillaMaxLevel.getValue()) {
+                            return enchantMaxLevel;
+                        } else if (hasCustomMaxLevel.getValue()) {
                             return highestLevel > customMaxLevel.getValue() ? customMaxLevel.getValue() : highestLevel;
                         } else {
                             return highestLevel;
