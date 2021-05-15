b318f05ebed54e5fa669ba311dce1baf879b5b80
StarWishsama <starwishsama@outlook.com>
2021-05-15 18:50:13 +0800

    fix(item): 修复智能接口丢失

--- src/main/java/io/github/thebusybiscuit/slimefun4/implementation/setup/SlimefunItemSetup.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/implementation/setup/SlimefunItemSetup.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.annotation.Nonnull;
 import javax.annotation.ParametersAreNonnullByDefault;
 
+import com.xzavier0722.mc.plugin.slimefun4.autocrafter.CrafterSmartPort;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
@@ -2619,6 +2620,12 @@ public final class SlimefunItemSetup {
                 .setProcessingSpeed(1)
                 .setEnergyConsumption(16)
                 .register(plugin);
+
+        new CrafterSmartPort(categories.cargo, SlimefunItems.CRAFTER_SMART_PORT, RecipeType.ENHANCED_CRAFTING_TABLE,
+                new ItemStack[]{
+                        null, SlimefunItems.CARGO_MOTOR, null,
+                        new ItemStack(Material.CHEST), SlimefunItems.CRAFTING_MOTOR, new ItemStack(Material.CHEST),
+                        null, SlimefunItems.ELECTRIC_MOTOR, null}).register(plugin);
 
         // @formatter:on
     }
