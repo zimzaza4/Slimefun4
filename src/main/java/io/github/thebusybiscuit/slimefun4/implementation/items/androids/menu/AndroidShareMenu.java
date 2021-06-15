fa34718573bab3e223cc601fe95e7eacf1ca5e1a
StarWishsama <starwishsama@outlook.com>
2021-06-15 12:45:14 +0800

    feat(menu): 展示信任玩家头颅物品

--- src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/androids/menu/AndroidShareMenu.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/androids/menu/AndroidShareMenu.java
@@ -8,10 +8,12 @@ import io.github.thebusybiscuit.cscorelib2.inventory.ChestMenu;
 import io.github.thebusybiscuit.cscorelib2.item.CustomItem;
 import io.github.thebusybiscuit.cscorelib2.skull.SkullItem;
 import io.github.thebusybiscuit.slimefun4.implementation.SlimefunPlugin;
+import io.github.thebusybiscuit.slimefun4.utils.ChatUtils;
 import io.github.thebusybiscuit.slimefun4.utils.HeadTexture;
 import me.mrCookieSlime.Slimefun.api.BlockStorage;
 import org.apache.commons.lang.Validate;
 import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 
@@ -37,21 +39,35 @@ public final class AndroidShareMenu {
 
     menu.setEmptySlotsClickable(false);
 
-    menu.addItem(1, new CustomItem(HeadTexture.SCRIPT_UP.getAsItemStack(), "", ""));
+    List<UUID> users = getTrustedUsers(b);
+
+    menu.addItem(1, new CustomItem(HeadTexture.SCRIPT_UP.getAsItemStack(), "&a添加信任玩家", ""));
     menu.addMenuClickHandler(1, (p1, slot, item, cursor, action) -> {
-      // @TODO
+      p1.closeInventory();
+      ChatUtils.awaitInput(p1, message -> {
+        Player target = Bukkit.getPlayerExact(message);
+
+        if (target == null) {
+          p1.sendMessage("找不到指定玩家 " + message);
+        } else {
+          addPlayer(p1, target, b, users);
+        }
+
+      });
       return false;
     });
-
-    List<UUID> users = getTrustedUsers(b);
 
     if (!users.isEmpty()) {
       List<UUID> displayUsers = users.subList(0 + (page - 1), Math.min(users.size() - 1, perPageDisplay));
       for (int index = 0; index < displayUsers.size(); index++) {
         int slot = index + startSlot;
-        menu.addItem(slot, SkullItem.fromPlayer(Bukkit.getOfflinePlayer(displayUsers.get(index))));
+        OfflinePlayer current = Bukkit.getOfflinePlayer(displayUsers.get(index));
+        menu.addItem(slot, new CustomItem(SkullItem.fromPlayer(current), "&b" + current.getName(), "&c左键 删除玩家"));
         menu.addMenuClickHandler(slot, (p1, slot1, item, cursor, action) -> {
-          // @TODO
+          if (action.isLeftClick()) {
+            removePlayer(p1, current.getPlayer(), b, users);
+          }
+
           return false;
         });
       }
@@ -66,6 +82,20 @@ public final class AndroidShareMenu {
 
   public static String generateEmptyList() {
     return gson.toJson(new ArrayList<UUID>());
+  }
+
+  private static void addPlayer(@Nonnull Player owner, @Nonnull Player p, @Nonnull Block android, @Nonnull List<UUID> users) {
+    boolean result = users.add(p.getUniqueId());
+    owner.sendMessage("添加玩家 " + p.getDisplayName() + " " + (result ? "成功" : "失败"));
+
+    BlockStorage.addBlockInfo(android, key, gson.toJson(users));
+  }
+
+  private static void removePlayer(@Nonnull Player owner, @Nonnull Player p, @Nonnull Block android, @Nonnull List<UUID> users) {
+    boolean result = users.remove(p.getUniqueId());
+    owner.sendMessage("删除玩家 " + p.getDisplayName() + " " + (result ? "成功" : "失败"));
+
+    BlockStorage.addBlockInfo(android, key, gson.toJson(users));
   }
 
   private static List<UUID> getTrustedUsers(@Nonnull Block b) {
