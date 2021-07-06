a385e4396d22570728e751adad71a89ea4d0a25a
StarWishsama <starwishsama@outlook.com>
2021-07-06 21:23:08 +0800

    fix(android): 添加丢失的提示, 修复菜单翻页显示异常

--- src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/androids/menu/AndroidShareMenu.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/androids/menu/AndroidShareMenu.java
@@ -37,6 +37,7 @@ public final class AndroidShareMenu {
   public static void openShareMenu(@Nonnull Player p, @Nonnull Block b, int page) {
     Validate.notNull(p, "The player cannot be null!");
     Validate.notNull(b, "The android block cannot be null!");
+    Validate.isTrue(page > 0, "The page must be above or equals 0!");
 
     ChestMenu menu = new ChestMenu(SlimefunPlugin.instance(), "&b共享权限管理");
 
@@ -44,7 +45,7 @@ public final class AndroidShareMenu {
 
     List<UUID> users = getTrustedUsers(b);
 
-    int pages = Math.max(1, users.size() / 36);
+    int pages = Math.max(0, users.size() / 36);
 
     // Draw background
 
@@ -60,18 +61,22 @@ public final class AndroidShareMenu {
 
     menu.addItem(0, new CustomItem(HeadTexture.SCRIPT_UP.getAsItemStack(), "&a添加信任玩家", ""));
     menu.addMenuClickHandler(0, (p1, slot, item, cursor, action) -> {
-      p1.closeInventory();
-      ChatUtils.awaitInput(p1, message -> {
-        Player target = Bukkit.getPlayerExact(message);
+          p1.closeInventory();
 
-        if (target == null) {
-          p1.sendMessage("找不到指定玩家 " + message);
-        } else {
-          addPlayer(p1, target, b, users);
-        }
+          p1.sendMessage("请输入欲添加的信任玩家 ID.");
 
-      });
-      return false;
+          ChatUtils.awaitInput(
+              p1,
+              message -> {
+                Player target = Bukkit.getPlayerExact(message);
+
+                if (target == null) {
+                  p1.sendMessage("找不到指定玩家 " + message);
+                } else {
+                  addPlayer(p1, target, b, users);
+                }
+              });
+          return false;
     });
 
     if (!users.isEmpty()) {
@@ -79,41 +84,47 @@ public final class AndroidShareMenu {
       for (int index = 0; index < displayUsers.size(); index++) {
         int slot = index + startSlot;
         OfflinePlayer current = Bukkit.getOfflinePlayer(displayUsers.get(index));
-        menu.addItem(slot, new CustomItem(SkullItem.fromPlayer(current), "&b" + current.getName(), "&c左键 删除玩家"));
-        menu.addMenuClickHandler(slot, (p1, slot1, item, cursor, action) -> {
-          if (action.isLeftClick()) {
-            removePlayer(p1, current, b, users);
-          }
+        menu.addItem(
+            slot,
+            new CustomItem(SkullItem.fromPlayer(current), "&b" + current.getName(), "&c左键 删除玩家"));
+        menu.addMenuClickHandler(
+            slot,
+            (p1, slot1, item, cursor, action) -> {
+              if (action.isLeftClick()) {
+                removePlayer(p1, current, b, users);
+              }
 
-          return false;
-        });
+              return false;
+            });
       }
     }
 
-    menu.addItem(46, ChestMenuUtils.getPreviousButton(p, page, pages));
-    menu.addMenuClickHandler(46, (pl, slot, item, cursor, action) -> {
-      int next = page - 1;
-      if (next < 1) next = pages;
-      if (next != page) {
-        openShareMenu(p, b, next);
-      }
-      return false;
-    });
+    if (pages > 0) {
+        menu.addItem(47, ChestMenuUtils.getPreviousButton(p, page, pages));
+        menu.addMenuClickHandler(46, (pl, slot, item, cursor, action) -> {
+                    int next = page - 1;
+                    if (next < 1) next = pages;
+                    if (next != page) {
+                        openShareMenu(p, b, next);
+                    }
+                    return false;
+        });
 
-    menu.addItem(50, ChestMenuUtils.getNextButton(p, page, pages));
-    menu.addMenuClickHandler(50, (pl, slot, item, cursor, action) -> {
-      int next = page + 1;
-      if (next > pages) {
-        next = 1;
-      }
-      if (next != page) {
-        openShareMenu(p, b, next);
-      }
-      return false;
-    });
+        menu.addItem(51, ChestMenuUtils.getNextButton(p, page, pages));
+        menu.addMenuClickHandler(50, (pl, slot, item, cursor, action) -> {
+                    int next = page + 1;
+                    if (next > pages) {
+                        next = 1;
+                    }
+                    if (next != page) {
+                        openShareMenu(p, b, next);
+                    }
+                    return false;
+        });
 
-    menu.open(p);
-  }
+        menu.open(p);
+        }
+    }
 
   public static String generateEmptyList() {
     return gson.toJson(new ArrayList<UUID>());
