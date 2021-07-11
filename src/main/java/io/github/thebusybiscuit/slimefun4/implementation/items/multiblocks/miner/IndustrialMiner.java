a37826fe7b1d9dbe665922ecae2b7379e8c6df96
ybw0014 <i@guizhanss.com>
2021-07-12 04:06:06 +0800

    移除重复代码

--- src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/multiblocks/miner/IndustrialMiner.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/multiblocks/miner/IndustrialMiner.java
@@ -116,27 +116,18 @@ public class IndustrialMiner extends MultiBlockMachine {
         Random random = ThreadLocalRandom.current();
 
         if (SlimefunPlugin.getMinecraftVersion().isAtLeast(MinecraftVersion.MINECRAFT_1_17)) {
-            // In 1.17, raw metals are obtained after breaking metal ores. Also, deepslate ores are added
+            // In 1.17, breaking metal ores should get raw metals. Also support deepslate ores.
             switch (ore) {
-                case COAL_ORE:
                 case DEEPSLATE_COAL_ORE:
                     return new ItemStack(Material.COAL);
-                case DIAMOND_ORE:
                 case DEEPSLATE_DIAMOND_ORE:
                     return new ItemStack(Material.DIAMOND);
-                case EMERALD_ORE:
                 case DEEPSLATE_EMERALD_ORE:
                     return new ItemStack(Material.EMERALD);
-                case REDSTONE_ORE:
                 case DEEPSLATE_REDSTONE_ORE:
                     return new ItemStack(Material.REDSTONE, 4 + random.nextInt(2));
-                case LAPIS_ORE:
                 case DEEPSLATE_LAPIS_ORE:
                     return new ItemStack(Material.LAPIS_LAZULI, 4 + random.nextInt(4));
-                case NETHER_QUARTZ_ORE:
-                    return new ItemStack(Material.QUARTZ);
-                case NETHER_GOLD_ORE:
-                    return new ItemStack(Material.GOLD_NUGGET,2 + random.nextInt(4));
                 case COPPER_ORE:
                 case DEEPSLATE_COPPER_ORE:
                     return new ItemStack(Material.RAW_COPPER);
@@ -146,48 +137,31 @@ public class IndustrialMiner extends MultiBlockMachine {
                 case GOLD_ORE:
                 case DEEPSLATE_GOLD_ORE:
                     return new ItemStack(Material.RAW_GOLD);
-                default:
-                    return new ItemStack(ore);
             }
-        } else if (SlimefunPlugin.getMinecraftVersion().isAtLeast(MinecraftVersion.MINECRAFT_1_16)) {
-            // In 1.16, nether gold ores are added
-            switch (ore) {
-                case COAL_ORE:
-                    return new ItemStack(Material.COAL);
-                case DIAMOND_ORE:
-                    return new ItemStack(Material.DIAMOND);
-                case EMERALD_ORE:
-                    return new ItemStack(Material.EMERALD);
-                case REDSTONE_ORE:
-                    return new ItemStack(Material.REDSTONE, 4 + random.nextInt(2));
-                case LAPIS_ORE:
-                    return new ItemStack(Material.LAPIS_LAZULI, 4 + random.nextInt(4));
-                case NETHER_QUARTZ_ORE:
-                    return new ItemStack(Material.QUARTZ);
-                case NETHER_GOLD_ORE:
-                    return new ItemStack(Material.GOLD_NUGGET,2 + random.nextInt(4));
-                default:
-                    // This includes Iron and Gold ore (and Ancient Debris)
-                    return new ItemStack(ore);
+        }
+        if (SlimefunPlugin.getMinecraftVersion().isAtLeast(MinecraftVersion.MINECRAFT_1_16)) {
+            // In 1.16, breaking nether gold ores should get gold nuggets
+            if (ore == Material.NETHER_GOLD_ORE) {
+                return new ItemStack(Material.GOLD_NUGGET, 2 + random.nextInt(4));
             }
-        } else {
-            switch (ore) {
-                case COAL_ORE:
-                    return new ItemStack(Material.COAL);
-                case DIAMOND_ORE:
-                    return new ItemStack(Material.DIAMOND);
-                case EMERALD_ORE:
-                    return new ItemStack(Material.EMERALD);
-                case REDSTONE_ORE:
-                    return new ItemStack(Material.REDSTONE, 4 + random.nextInt(2));
-                case LAPIS_ORE:
-                    return new ItemStack(Material.LAPIS_LAZULI, 4 + random.nextInt(4));
-                case NETHER_QUARTZ_ORE:
-                    return new ItemStack(Material.QUARTZ);
-                default:
-                    // This includes Iron and Gold ore (and Ancient Debris)
-                    return new ItemStack(ore);
-            }
+        }
+
+        switch (ore) {
+            case COAL_ORE:
+                return new ItemStack(Material.COAL);
+            case DIAMOND_ORE:
+                return new ItemStack(Material.DIAMOND);
+            case EMERALD_ORE:
+                return new ItemStack(Material.EMERALD);
+            case REDSTONE_ORE:
+                return new ItemStack(Material.REDSTONE, 4 + random.nextInt(2));
+            case LAPIS_ORE:
+                return new ItemStack(Material.LAPIS_LAZULI, 4 + random.nextInt(4));
+            case NETHER_QUARTZ_ORE:
+                return new ItemStack(Material.QUARTZ);
+            default:
+                // This includes Iron and Gold ore (and Ancient Debris)
+                return new ItemStack(ore);
         }
     }
 
