d003c87160793a67db593f33a55a51c670d8ecea
zimzaza4 <1875162740@qq.com>
2022-06-03 18:31:10 +0800

    redis

--- src/main/java/io/github/thebusybiscuit/slimefun4/api/player/RedisPlayerData.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/api/player/RedisPlayerData.java
@@ -10,6 +10,7 @@ import redis.clients.jedis.Jedis;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 public class RedisPlayerData {
     public final static String MESSAGE = "ASK_FOR_ASYNC_DATA";
@@ -18,9 +19,18 @@ public class RedisPlayerData {
             @Override
             public void run() {
                 Jedis jedis = RedisService.getPool().getResource();
+                Set<String> data = jedis.smembers("ResearchData:" + profile.getUUID());
                 for (Research r : profile.getResearches()) {
-                    if (!jedis.smembers("ResearchData:" + profile.getUUID()).contains(r.getKey().toString())) {
+                    if (!data.contains(r.getKey().toString())) {
                         jedis.sadd("ResearchData:" + profile.getUUID(), r.getKey().toString());
+                    }
+                }
+                for (String s : data) {
+                    Optional<Research> research = Research.getResearch(NamespacedKey.fromString(s));
+                    if (research.isPresent()) {
+                        if (!profile.getResearches().contains(research.get())) {
+                            jedis.srem("ResearchData:" + profile.getUUID(), s);
+                        }
                     }
                 }
                 jedis.close();
